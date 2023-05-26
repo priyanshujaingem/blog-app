cc64c594274d8b707d42ef422b628188f71438f3
priyanshujaingem <priyanshu.jain@geminisolutions.com>
2023-05-26 15:17:17 +0530

    Title toggle refactor

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styles from './card.module.css';
 
 function Card({ title, body, id }) {
-  const handleAccordian = (event) => {
-    let doc = document.getElementById(`body${event.target.id}`);
-    if (doc.style.display === 'none') doc.style.display = 'block';
-    else doc.style.display = 'none';
-  };
+  const [isActive, setIsActive] = useState(true);
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title} onClick={handleAccordian} id={id}>
+      <h1
+        className={styles.title}
+        onClick={() => setIsActive(!isActive)}
+        id={id}
+      >
         {title}
       </h1>
-      <p id={`body${id}`} className={styles.cardBody}>
-        {body}
-      </p>
+      {isActive && (
+        <p id={`body${id}`} className={styles.cardBody}>
+          {body}
+        </p>
+      )}
     </div>
   );
 }
