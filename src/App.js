71a9ad985d6052e4a0dcc76bcb92fe1fd7c83452
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2023-05-26 14:02:14 +0530

    Axios and Get Post API call done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { getPosts } from './services/api';
+import { useState } from 'react';
 
 function App() {
+  const [post, setPost] = useState([]);
+  const getPostsData = async () => {
+    let postData = await getPosts();
+    if (postData?.data?.length > 0) {
+      setPost(postData?.data);
+    }
+  }
+
+  useEffect(() => {
+    getPostsData();
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
